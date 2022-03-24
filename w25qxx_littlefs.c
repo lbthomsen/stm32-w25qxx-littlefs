61d0ce2a1194f2997d1fd61ebb82ae5326135a4d
Lars Boegild Thomsen <lth@netcompartner.com>
2022-03-25 07:16:12 +0800

    Hmmmm

--- w25qxx_littlefs.c
+++ w25qxx_littlefs.c
@@ -12,7 +12,7 @@
 #include "w25qxx_littlefs.h"
 
 int littlefs_read(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size);
-int littlefs_prog(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size);
+int littlefs_prog(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size);
 int littlefs_erase(const struct lfs_config *c, lfs_block_t block);
 int littlefs_sync(const struct lfs_config *c);
 
@@ -24,8 +24,8 @@ struct lfs_config littlefs_config = {
     .sync  = littlefs_sync,
 
     // block device configuration
-    .read_size = 256,
-    .prog_size = 256,
+    .read_size = 32,
+    .prog_size = 32,
     .block_size = 4096,
     .block_count = 128,
     .cache_size = 128,
@@ -65,9 +65,9 @@ int littlefs_read(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, 
 	return 0;
 }
 
-int littlefs_prog(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size) {
+int littlefs_prog(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size) {
 	LFS_DBG("LittleFS Prog b = 0x%04lx o = 0x%04lx s = 0x%04lx", block, off, size);
-	if (w25qxx_write(w25qxx_handle, block * w25qxx_handle->sector_size + off, buffer, size) != W25QXX_Ok) return -1;
+	if (w25qxx_write(w25qxx_handle, block * w25qxx_handle->sector_size + off, (void *)buffer, size) != W25QXX_Ok) return -1;
 	return 0;
 }
 
